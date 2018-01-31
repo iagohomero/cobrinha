e9d24568a6ba9932bfe69b9bca89366b4fe52c34
Iago Homero <iagohomero@gmail.com>
2018-01-30 22:44:14 -0200

    changes

--- js/game.js
+++ js/game.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
   
     var matrix = initGame();
+    var direction = null;
+    
+    var interval = window.setInterval(function(){ move(null, matrix, direction, interval) },  100);
 
     $(document).keydown(function (e) {
-        pressKey(e, matrix);
+      direction = move(e, matrix, null, interval);
     });
 
+    // window.setInterval(function(){
+    //   var interval = window.setInterval(function(){ move(null, matrix, direction, interval) },  500);
+    // }, 1);
 });
 
 function initGame() {
@@ -18,7 +24,7 @@ function initGame() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 'f', 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,8 +50,10 @@ function initGame() {
 
     for(j = 0; j < column; j++){
       var cell = tableRow.insertCell(j);
-      //cell.innerHTML = matrix[i][j];
-      if(matrix[i][j] == 1){
+      if(matrix[i][j] == "f"){
+        cell.className = "fruit";
+      }
+      else if(matrix[i][j] != 0){
         cell.className = "snake";
       }
       else{
@@ -57,75 +65,168 @@ function initGame() {
   return matrix;
 }
 
-function pressKey(event, matrix) {
-    var key = event.keyCode;
-
-    console.log(key);
+function move(event, matrix, direction, interval) {
+    if(event != null){
+      var key = event.keyCode;
+    }
 
     var row = matrix.length;
     var column = matrix[0].length;
-    var position = [];
+    var position = getSnakePosition(matrix);
 
-    // for (i = 0; i < row; i++) { 
-    //   for(j = 0; j < column; j++){
-    //       if(matrix[i][j] == 1){
-    //           position.push([i, j]);
-    //       }
-    //   }
-    // }
-    for (i = 0; i < row; i++) { 
-      for(j = 0; j < column; j++){
-          if(matrix[i][j] == 1){
-              position = [i, j];
-          }
+    try {
+      if(key == 37 || key == 65 || direction == "left"){
+        // console.log("KEY left pressed");
+        moveLeft(matrix, position); 
+        direction = "left";
       }
-    }
-    
-    if(key == 37 || key == 65){
-      if(position[1] != 0){
-        console.log("KEY left pressed");
-        matrix[position[0]][position[1]] = 0;
-        matrix[position[0]][position[1] - 1] = 1;
+      else if(key == 38 || key == 87 || direction == "up"){
+        // console.log("KEY up pressed");  
+        moveUp(matrix, position); 
+        direction = "up";
       }
-        // for (i = 0; i < position.length; i++) { 
-        //   matrix[position[i][0]][position[i][1]] = 0;
-        //   matrix[position[i][0]][position[i][1] - 1] = 1;
-        // }
-    }
-    else if(key == 38 || key == 87){
-      if(position[0] != 0){
-        console.log("KEY up pressed");
-        matrix[position[0]][position[1]] = 0;
-        matrix[position[0] - 1][position[1]] = 1;
+      else if(key == 39 || key == 68 || direction == "right"){
+        // console.log("KEY right pressed");
+        moveRight(matrix, position); 
+        direction = "right";
       }
-        // for (i = 0; i < position.length; i++) { 
-        //   matrix[position[i][0]][position[i][1]] = 0;
-        //   matrix[position[i][0] - 1][position[i][1]] = 1;
-        // }
-    }
-    else if(key == 39 || key == 68){
-      if(position[1] != column - 1){
-        console.log("KEY right pressed");
-        matrix[position[0]][position[1]] = 0;
-        matrix[position[0]][position[1] + 1] = 1;
-      } 
-        // for (i = 0; i < position.length; i++) { 
-        //   matrix[position[i][0]][position[i][1]] = 0;
-        //   matrix[position[i][0]][position[i][1] + 1] = 1;
-        // }
-    }
-    else if(key == 40 || key == 83){
-      if(position[0] != row - 1){
-        console.log("KEY down pressed");
-        matrix[position[0]][position[1]] = 0;
-        matrix[position[0] + 1][position[1]] = 1;
+      else if(key == 40 || key == 83 || direction == "down"){
+        // console.log("KEY down pressed");
+        moveDown(matrix, position); 
+        direction = "down";
       }
-        // for (i = 0; i < position.length; i++) { 
-        //   matrix[position[i][0]][position[i][1]] = 0;
-        //   matrix[position[i][0] + 1][position[i][1]] = 1;
-        // }
+    }catch(err) {
     }
     updateTable(matrix);
+    console.log(matrix);
+    //clearInterval(interval);
+    return direction;
+}
+
+function moveLeft(matrix, position){
+  var aux = null;
+  if(matrix[position[0][1][0]][position[0][1][1] - 1] == 0 || matrix[position[0][1][0]][position[0][1][1] - 1] == "f"){
+    for (i = 0; i < position.length; i++) {
+      if(aux == null){
+        if(matrix[position[0][1][0]][position[0][1][1] - 1] == "f"){
+          matrix[position[0][1][0]][position[i][1][1] - 1] = matrix[position[0][1][0]][position[i][1][1]] + 1;
+          generateFruit(matrix);
+          break;  
+        }
+        else{
+          matrix[position[i][1][0]][position[i][1][1] - 1] = matrix[position[i][1][0]][position[i][1][1]];  
+          matrix[position[i][1][0]][position[i][1][1]] = 0;  
+        }  
+      }
+      else{
+        matrix[aux[0]][aux[1]] = matrix[position[i][1][0]][position[i][1][1]];
+        matrix[position[i][1][0]][position[i][1][1]] = 0;
+      }
+      aux = [position[i][1][0], position[i][1][1]];
+    }
+  }
+}
+
+function moveUp(matrix, position){
+  var aux = null;
+  if(matrix[position[0][1][0] - 1][position[0][1][1]] == 0 || matrix[position[0][1][0] - 1][position[0][1][1]] == "f"){
+    for (i = 0; i < position.length; i++) {
+      if(aux == null){
+        if(matrix[position[0][1][0] - 1][position[0][1][1]] == "f"){
+          matrix[position[0][1][0] - 1][position[i][1][1]] = matrix[position[0][1][0]][position[i][1][1]] + 1;
+          generateFruit(matrix);
+          break;  
+        }
+        else{ 
+          matrix[position[i][1][0] - 1][position[i][1][1]] = matrix[position[i][1][0]][position[i][1][1]];  
+          matrix[position[i][1][0]][position[i][1][1]] = 0;
+        }
+      }
+      else{
+        matrix[aux[0]][aux[1]] = matrix[position[i][1][0]][position[i][1][1]];
+        matrix[position[i][1][0]][position[i][1][1]] = 0;
+      }
+      aux = [position[i][1][0], position[i][1][1]];
+    }
+  }
+}
+
+function moveRight(matrix, position){
+  var aux = null;
+  if(matrix[position[0][1][0]][position[0][1][1] + 1] == 0 || matrix[position[0][1][0]][position[0][1][1] + 1] == "f"){
+    for (i = 0; i < position.length; i++) {
+      if(aux == null){
+        if(matrix[position[0][1][0]][position[0][1][1] + 1] == "f"){
+          matrix[position[0][1][0]][position[0][1][1] + 1] = matrix[position[0][1][0]][position[i][1][1]] + 1;
+          generateFruit(matrix);
+          break;  
+        }
+        else{
+          matrix[position[i][1][0]][position[i][1][1] + 1] = matrix[position[i][1][0]][position[i][1][1]];
+          matrix[position[i][1][0]][position[i][1][1]] = 0;
+        }
+      }
+      else{
+        matrix[aux[0]][aux[1]] = matrix[position[i][1][0]][position[i][1][1]];
+        matrix[position[i][1][0]][position[i][1][1]] = 0;
+      }
+      aux = [position[i][1][0], position[i][1][1]];
+    }
+  } 
+}
+
+function moveDown(matrix, position){
+  var aux = null;
+  if(matrix[position[0][1][0] + 1][position[0][1][1]] == 0 || matrix[position[0][1][0] + 1][position[0][1][1]] == "f"){
+    for (i = 0; i < position.length; i++) {
+      if(aux == null){
+        if(matrix[position[0][1][0] + 1][position[0][1][1]] == "f"){
+          matrix[position[i][1][0] + 1][position[i][1][1]] = matrix[position[i][1][0]][position[i][1][1]] + 1;
+          generateFruit(matrix);
+          break;  
+        }
+        else{
+          matrix[position[i][1][0] + 1][position[i][1][1]] = matrix[position[i][1][0]][position[i][1][1]];  
+          matrix[position[i][1][0]][position[i][1][1]] = 0;
+        }
+      }
+      else{
+        matrix[aux[0]][aux[1]] = matrix[position[i][1][0]][position[i][1][1]];
+        matrix[position[i][1][0]][position[i][1][1]] = 0;
+      }
+      aux = [position[i][1][0], position[i][1][1]];
+    }
+  }
+}
+
+function getSnakePosition(matrix){
+  var row = matrix.length;
+  var column = matrix[0].length;
+  var position = [];
+  for (i = 0; i < row; i++) { 
+    for(j = 0; j < column; j++){
+        if(matrix[i][j] != 0 && matrix[i][j] != "f"){
+          position.push([ [matrix[i][j]] , [i, j] ]);
+        }
+    }
+  }
+  position.sort(sortFunction);
+  return position;
+}
+
+function generateFruit(matrix) {
+  var row = matrix.length;
+  var column = matrix[0].length;
+
+  row = Math.floor(Math.random() * row) + 0;  
+  column = Math.floor(Math.random() * column) + 0;
+
+  if(matrix[row][column] == 0){
+    matrix[row][column] = "f";
+  }
+  else{
+    generateFruit(matrix);
+  }
 }
 
 function updateTable(matrix){
@@ -137,9 +238,10 @@ function updateTable(matrix){
   for (i = 0; i < row; i++) { 
     for(j = 0; j < column; j++){
       cell = table.rows[i].cells[j];
-      // cell.innerHTML = matrix[i][j];
-
-      if(matrix[i][j] == 1){
+      if(matrix[i][j] == "f"){
+        cell.className = "fruit";
+      }
+      else if(matrix[i][j] != 0){
         cell.className = "snake";
       }
       else{
@@ -147,6 +249,15 @@ function updateTable(matrix){
       }
     }
   }
+}
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? 1 : -1;
+    }
 }
 
 
